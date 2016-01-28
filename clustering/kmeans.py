74477d094125053d3d65d074abc8e55287413a03
Saeed <masoudian.s@gmail.com>
2016-01-29 03:27:54 +0330

    full clustering & label clusters & add indexind,clustering files to git

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -1,15 +1,19 @@
+import json
 from math import sqrt
+import pickle
 import random
 from pprint import pprint
 __author__ = 'chester'
 
-number_of_iteration = 10
+number_of_iteration = 20
 
 def clustering(vectors):
     costs = []
     clusters = []
-    for i in range(len(vectors)):
+    for i in range(60):
         curr_cluster = accurate_k_means(vectors, i+1)
+        print('one cluster iteration ' + str(i))
+        print('-----------------------------')
         costs.append(curr_cluster['cost'])
         clusters.append(curr_cluster)
     print(costs)
@@ -26,17 +30,18 @@ def clustering(vectors):
         diff.append((costs[i+1]-2*costs[i]+costs[i-1]))
     print(diff)
     minimum = min(diff,)
-    cluster_number = 0
+    cluster_number = 1
     for i in range(len(diff)):
         if diff[i] == minimum:
             cluster_number = i+2
-    return {'cluster_number':cluster_number, 'cluster':clusters[cluster_number-1]}
+    return {'cluster_number':cluster_number, 'cluster_1':clusters[cluster_number-1], 'cluster_2':clusters[cluster_number-2]}
 
 
 def accurate_k_means(vectors, k):
     curr_cluster = k_means(vectors, k)
     for i in range(number_of_iteration):
         new_cluster = k_means(vectors, k)
+        print('intra cluster iteration : ' + str(i))
         if new_cluster['cost'] < curr_cluster['cost']:
             curr_cluster = new_cluster
     return curr_cluster
@@ -49,7 +54,7 @@ def k_means(vectors, k):
     current_cluster = []
     for i in range(len(vectors)):
         current_cluster.append(0)
-    while delta_cost != 0:
+    while delta_cost > 0.001:
         if start:
             current_proxies = new_proxies(vectors, current_cluster, k)
         else:
@@ -59,12 +64,6 @@ def k_means(vectors, k):
         new_cost = cost(vectors, current_proxies, current_cluster)
         delta_cost = current_cost - new_cost
         current_cost = new_cost
-        #print('-----------------------------------')
-        #print('delta : ')
-        #print(delta_cost)
-        #print('new : ')
-        #print(current_cost)
-        #print(current_cluster)
     out = {'cost': current_cost, 'cluster' : current_cluster}
     return out
 
@@ -76,10 +75,7 @@ def cost(vectors, proxies, cluster):
     return cost
 
 def subtract(a, b):
-    out = []
-    for i in range(len(b)):
-        out.append(a[i]-b[i])
-    return out
+    return sum_vector(a, b, -1)
 
 def benefit(vectors, proxies, cluster):
     benefit = 0
@@ -89,12 +85,9 @@ def benefit(vectors, proxies, cluster):
 
 def new_proxies(vectors, cluster, k):
     proxies = []
-    zero_vector = []
-    for i in range(len(vectors[0]['vector'])):
-        zero_vector.append(0)
     count_cluster = []
     for i in range(k):
-        proxies.append(zero_vector)
+        proxies.append({})
         count_cluster.append(0)
     for i in range(len(cluster)):
         count_cluster[cluster[i]] += 1
@@ -106,9 +99,9 @@ def new_proxies(vectors, cluster, k):
     return proxies
 
 def sum_vector(a, b, devide_factor):
-    sum_vector = []
-    for i in range(len(a)):
-        sum_vector.append(a[i]+ b[i]/devide_factor)
+    sum_vector = { k: a.get(k, 0) + b.get(k, 0)/devide_factor for k in set(a) | set(b) }
+    #for i in range(len(a)):
+    #    sum_vector.append(a[i]+ b[i]/devide_factor)
     return sum_vector
 def search_nearest(doc, current_proxies):
     maximum = -1
@@ -122,24 +115,26 @@ def search_nearest(doc, current_proxies):
 
 def size(vector):
     sum = 0
-    for i in vector:
-        sum += i*i
+    for term in vector:
+        sum += vector[term]*vector[term]
     return sqrt(sum)
 
 def dist(a, b):
     dist = 0
-    for i in range(len(a)):
-        dist += a[i]*b[i]
+    for term in set(a) & set(b):
+        dist += a[term]*b[term]
     return dist
 
 def initial_points(vectors, k):
     random_indexes = []
     init_points = []
-    for i in range(1, k+1):
-        r = random.randrange(0,len(vectors))
-        while (r in random_indexes):
-            r = random.randrange(0,len(vectors))
-        random_indexes.append(r)
+    all_indexes = []
+    for i in range(len(vectors)):
+        all_indexes.append(i)
+    for i in range(k):
+        r = random.randrange(0,len(all_indexes))
+        random_indexes.append(all_indexes[r])
+        all_indexes.remove(all_indexes[r])
     for r in random_indexes:
         temp = normalize(vectors[r]['vector'])
         init_points.append(temp)
@@ -147,17 +142,25 @@ def initial_points(vectors, k):
 
 def normalize(vector):
     vector_size = size(vector)
-    temp = []
     if vector_size == 0:
         return vector
-    for i in range(len(vector)):
-        temp.append(vector[i]/vector_size)
-    return temp
-def main():
-    vectors = [{'vector':[2,2,2 ,2], 'id':1},{'vector':[1,1,1 , 3], 'id':2},{'vector':[1,3,3, 3], 'id':3}, {'vector':[1,10,3, 10], 'id':3}, {'vector':[10,1,30, 10], 'id':3}]
+    for term in vector:
+        vector[term] /= vector_size
+    return vector
+def load_vectors():
+    with open('../vectors.txt', 'rb') as f:
+        return pickle.load(f)
+def normalize_collection(vectors):
     for vec in vectors:
         vec['vector'] = normalize(vec['vector'])
-    #print(dist(vectors[0]['vector'],vectors[1]['vector']))
-    pprint(clustering(vectors))
+def main():
+    #vectors = [{'vector':{'salam':2,'khiar':2, 'havij':1},'id':0},{'vector':{'salam':2,'saeed':1},'id':1},{'vector':{'hamid':2,'saeed':1,'salam':1},'id':2},{'vector':{'havij':2,'ali':0.5,'salam':1},'id':3},{'vector':{'khiar':1,'ali':5,'salam':1},'id':4}]
+    vectors = load_vectors()[0]
+    normalize_collection(vectors)
+    result = clustering(vectors)
+    pprint(result)
+    with open('clustering.json', 'w') as json_file:
+            json.dump(result, json_file)
+
 if __name__ == '__main__':
     main()
--- elastic/elastic.py
+++ elastic/elastic.py
@@ -1,5 +1,8 @@
+import json
 from math import log
+import pickle
 from crawler.parser import parse_html
+import os
 
 __author__ = 'chester'
 from elasticsearch import Elasticsearch
@@ -7,24 +10,27 @@ from pprint import pprint
 
 doc_vectors = dict()
 es = Elasticsearch()
-ids = []
 tf_idf_vectors = []
 title_priority = 3
 
-def index(docs_list):
+def index():
+    docs_list = load_docs()
     es.indices.delete(index='researchgate_index', ignore=[400, 404])
     es.indices.create(index='researchgate_index', ignore=400)
+    ids = []
     for doc in docs_list:
         id = doc['id']
         es.index(index='researchgate_index', doc_type='paper', id=id, body=doc)
         ids.append(id)
+    write_ids(ids)
 
 def collection_vectorize():
+    ids = read_ids()
     vectors = []
     for id in ids:
         vectors.append(get_term_vector(es,id))
     vocab = compute_terms(vectors)
-    print(vocab)
+    pprint(len(vocab))
     for vector in vectors:
         tf_idf_vector = dict()
         raw_vector = vector['abstract']
@@ -32,7 +38,32 @@ def collection_vectorize():
         raw_vector = vector['title']
         tf_idf_vector['title'] = tf_idf_vectorize(vocab, raw_vector, len(vectors))
         tf_idf_vectors.append(tf_idf_vector)
-    return finalize_vectors(tf_idf_vectors, vocab, ids)
+    pprint(tf_idf_vectors[0])
+    return finalize_json_vectors(tf_idf_vectors, ids), vocab
+    #return finalize_vectors(tf_idf_vectors, vocab, ids), vocab
+
+def finalize_json_vectors(tf_idf_vectors, ids):
+    out_vectors = []
+    for i in range(len(tf_idf_vectors)):
+        out_vectors.append({'vector':{}, 'id':ids[i]})
+    for i in range(len(tf_idf_vectors)):
+        vec = tf_idf_vectors[i]
+        terms = []
+        for term in vec['abstract']:
+            terms.append(term)
+        for term in vec['title']:
+            if term not in vec['abstract']:
+                terms.append(term)
+
+        for term in terms:
+            a = 0
+            b = 0
+            if term in vec['title']:
+                a = vec['title'][term]
+            if term in vec['abstract']:
+                b = vec['abstract'][term]
+            out_vectors[i]['vector'][term] = a*title_priority + b
+    return out_vectors
 
 def finalize_vectors(tf_idf_vectors, vocab, ids):
     out_vectors = []
@@ -46,16 +77,21 @@ def finalize_vectors(tf_idf_vectors, vocab, ids):
     return out_vectors
 def tf_idf_vectorize(vocab, raw_vector, length):
     tf_idf_vector = dict()
-    for term in vocab:
+
+    for term in raw_vector:
         idf = log((length+1)/vocab[term])
-        tf = 0
-        if term in raw_vector:
-            tf = raw_vector[term]['tf']
+        tf = raw_vector[term]['tf']
         tf_idf_vector[term] = idf*tf
+    #for term in vocab:
+    #    idf = log((length+1)/vocab[term])
+    #    tf = 0
+    #    if term in raw_vector:
+    #        tf = raw_vector[term]['tf']
+    #    tf_idf_vector[term] = idf*tf
     return tf_idf_vector
 
 def compute_terms(vectors):
-    vocab = {}
+    vocab = dict()
     for doc_vector in vectors:
         abs_vec = doc_vector['abstract']
         title_vec = doc_vector['title']
@@ -95,19 +131,35 @@ def vectorize(general_vector, type):
     return temp
 
 def load_docs():
-    with open('../crawler/html.txt', 'r') as f:
-        html = f.read()
-    doc = parse_html(html, 112)
-    a = {'id' : 1,'abstract' : 'salam salam saeed xx', 'title' : 'xx salam inja bedoone man'}
-    b = {'id' : 2,'abstract' : 'salam hamid', 'title' : 'salam inja bedoone to'}
-    c = {'id' : 3,'abstract' : 'salam hamid', 'title' : 'salam inja bedoone to'}
-    return [a,b,c]
+    docs = []
+    path = os.path.join(os.path.dirname(__file__).replace('elastic', ''), 'all_jsons')
+    print(path)
+    for filename in os.listdir(path):
+        with open(path + '/' + filename, 'r') as f:
+            doc = dict()
+            real_doc = json.loads(f.read())
+            doc['abstract'] = real_doc['datas']['abstract']
+            doc['title'] = real_doc['datas']['title']
+            doc['id'] = real_doc['datas']['publication_uid']
+            docs.append(doc)
+    return docs
+def write_ids(ids):
+    with open('../ids.txt', 'wb') as f:
+        pickle.dump(ids, f)
+def read_ids():
+    with open('../ids.txt', 'rb') as f:
+        ids = pickle.load(f)
+    return ids
+
+def write_vectors(vectors):
+    with open('../vectors.txt', 'wb') as f:
+        pickle.dump(vectors, f)
 
 def main():
-    docs = load_docs()
-    index(docs)
-    pprint(collection_vectorize())
-
+    print('hello')
+    #index()
+    #vectors = collection_vectorize()
+    #write_vectors(vectors)
 
 if __name__ == '__main__':
     main()
